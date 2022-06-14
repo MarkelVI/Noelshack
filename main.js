a6314c03daab98432e0ed390f17fd43028aca051
MarkelVI <99678734+MarkelVI@users.noreply.github.com>
2022-06-14 23:30:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getLink() {
   const page = document.getElementById('lien').value;
   var isse = page.substring(37, 42).replace("/", "-");
-  var pagent = `https://www.noelshack.com/${issent}${page[47:]}`;
+  var pagent = `https://www.noelshack.com/${issent}${page[47:]}`
 
   document.getElementById("answer").innerHTML = pagent;
 }
