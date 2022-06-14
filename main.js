5ba28e5942797cd147a547505492d1601c9f1ccd
MarkelVI <99678734+MarkelVI@users.noreply.github.com>
2022-06-15 00:17:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function getLink() {
   const page = document.getElementById('lien').value;
-  var isse = page.substring(/37/g, 47).replace("/", "-");
+  var isse = page.substring(37, 47).replace(///g, "-");
   var pagent = `https://www.noelshack.com/${isse}${page.substring(47)}`;
 
   document.getElementById("answer").innerHTML = pagent;
